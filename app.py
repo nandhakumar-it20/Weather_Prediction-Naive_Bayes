e6260473076c876dc502b18771b9935e98774d08
nandhakumars4757 <113059991+nandhakumars4757@users.noreply.github.com>
2022-09-08 23:01:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pandas as pd
 import time
+from sklearn import datasets
+from sklearn import metrics
+from sklearn.naive_bayes import GaussianNB, MultinomialNB
+from sklearn.datasets import fetch_openml
 
 st.title('Weather Prediction using Python')
 st.caption('Predicting Weather using the uploaded csv file')
@@ -8,11 +12,10 @@ st.info("Developed by NANDHAKUMAR S, SUJITH V, MOHAMED RAFEEK S, DHIVAKAR S [Dai
 st.snow()
 with st.spinner('Loading...'):
     time.sleep(1)
-uploaded_files = st.file_uploader("Choose a CSV file to process", type='csv')
-if uploaded_files is not None:
-     bytes_data = uploaded_files.read()
-     st.write("filename:", uploaded_files.name)
-
+    
+weather = datasets.fetch_openml(name='weather', version=2)
+st.write('Features:',   weather.feature_names)
+st.write('Target(s):',  weather.target_names)
     
 
 
