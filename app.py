30a07df89561181bbbe5e55391531f3225b06ee7
nandhakumars4757 <113059991+nandhakumars4757@users.noreply.github.com>
2022-09-08 23:18:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,19 +7,23 @@ from sklearn import metrics
 from sklearn.naive_bayes import GaussianNB, MultinomialNB
 from sklearn.datasets import fetch_openml
 
+
 with st.spinner('Loading...'):
     time.sleep(1)
 st.title('Weather Prediction using Python')
 st.caption('Predicting Weather using the uploaded csv file')
 st.info("Developed by NANDHAKUMAR S, SUJITH V, MOHAMED RAFEEK S, DHIVAKAR S [Daisi Hackathon]")
 st.snow()
-   
+  
+    
 weather = datasets.fetch_openml(name='weather', version=2)
 st.write('Features:',   weather.feature_names)
 st.write('Target(s):',  weather.target_names)
 
+
 df = pd.DataFrame( np.c_[weather.data, weather.target],columns=np.append(weather.feature_names, weather.target_names) )
 df
+
 
 st.write('Values:')
 st.write(df['outlook'])
@@ -28,6 +32,7 @@ st.write( df['outlook'].value_counts() )
 st.write('\nFrequencies grouped by target:')
 st.write( df.groupby(['play','outlook']).size() )
 
+
 st.write('Values:')
 st.write(df['windy'])
 st.write('\nFrequencies:')
@@ -35,10 +40,12 @@ st.write(df['windy'].value_counts())
 st.write('\nFrequencies grouped by target:')
 st.write( df.groupby(['play','windy']).size() )
 
+
 st.write('Values:')
 st.write(df['play'])
 st.write('\nFrequencies:')
 st.write(df['play'].value_counts())
+
 
 st.write( 'Instances where play=yes:')
 st.write( df.query('play == "yes"') )
@@ -52,6 +59,25 @@ meanNtemp = df.query('play == "no"').temperature.mean()
 stdNtemp  = df.query('play == "no"').temperature.std()
 st.write( '\nmean temperature (play=no)  ={:10.6f}'.format(meanNtemp) )
 st.write( ' std temperature (play=no)  ={:10.6f}'.format( stdNtemp) )
+
+
+st.write( 'Instances where play=yes:')
+st.write( df.query('play == "yes"') )
+meanYhumd = df.query('play == "yes"').humidity.mean()
+stdYhumd  = df.query('play == "yes"').humidity.std()
+st.write( '\nmean humidity (play=yes) ={:10.6f}'.format(meanYhumd) )
+st.write( ' std humidity (play=yes) ={:10.6f}'.format( stdYhumd) )
+st.write( '\nInstances where play=no:')
+st.write( df.query('play == "no"') )
+meanNhumd = df.query('play == "no"').humidity.mean()
+stdNhumd  = df.query('play == "no"').humidity.std()
+st.write( '\nmean humidity (play=no)  ={:10.6f}'.format(meanNhumd) )
+st.write( ' std humidity (play=no)  ={:10.6f}'.format( stdNhumd) )
+
+category_columns = ['outlook', 'windy', 'play']
+df[ category_columns] = df[ category_columns].apply(lambda x: pd.factorize(x)[0])
+df
+
 
 
     
