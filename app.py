562b112c48ee2156de7f74b293ac76fbb9ee77e0
nandhakumars4757 <113059991+nandhakumars4757@users.noreply.github.com>
2022-09-08 23:05:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,16 +6,31 @@ from sklearn import metrics
 from sklearn.naive_bayes import GaussianNB, MultinomialNB
 from sklearn.datasets import fetch_openml
 
+with st.spinner('Loading...'):
+    time.sleep(1)
 st.title('Weather Prediction using Python')
 st.caption('Predicting Weather using the uploaded csv file')
 st.info("Developed by NANDHAKUMAR S, SUJITH V, MOHAMED RAFEEK S, DHIVAKAR S [Daisi Hackathon]")
 st.snow()
-with st.spinner('Loading...'):
-    time.sleep(1)
     
 weather = datasets.fetch_openml(name='weather', version=2)
 st.write('Features:',   weather.feature_names)
 st.write('Target(s):',  weather.target_names)
     
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    if __name__ == "__main__":
+    # render the app using streamlit ui function
+    st_ui()
 
