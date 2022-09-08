784d2efbebfe649424c73e7b97739d978bbd45a6
nandhakumars4757 <113059991+nandhakumars4757@users.noreply.github.com>
2022-09-08 22:53:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.caption('Predicting Weather using the uploaded csv file')
 st.info("Developed by NANDHAKUMAR S, SUJITH V, MOHAMED RAFEEK S, DHIVAKAR S [Daisi Hackathon]")
 st.snow()
 with st.spinner('Loading...'):
-    time.sleep(1.5)
+    time.sleep(1)
 uploaded_files = st.file_uploader("Choose a CSV file to process", type='csv')
 if uploaded_files is not None:
      bytes_data = uploaded_files.read()
